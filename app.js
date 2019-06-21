4c52f752aed23b7fe16a1b7de7bef2f154513a3b
Drumil-SA <52069938+Drumil-SA@users.noreply.github.com>
2019-06-21 16:44:01 +0530

    Add files via upload
    add .env file

--- app.js
+++ app.js
@@ -3,35 +3,41 @@ var app = express();
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var Userdata = require("./models/data.js");
+var dotenv = require('dotenv');
+dotenv.config();
+app.set("view engine", "ejs");
 
-app.set("view engine","ejs");
-mongoose.connect("mongodb://localhost:27017/List",{useNewUrlParser:true});
+var DB = process.env.DB;
+var DB_host = process.env.DB_host;
+var DB_port = process.env.DB_port;
+var DB_name = process.env.DB_name;
+mongoose.connect(DB+"://"+DB_host+":"+DB_port+"/"+DB_name, { useNewUrlParser: true });
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
-app.get("/",function(req,res){
-    Userdata.find({},function(err,allData){
-        if(err){
+app.get("/", function (req, res) {
+    Userdata.find({}, function (err, allData) {
+        if (err) {
             console.log(err);
-        }else{
-           res.render("home",{userData : allData});
+        } else {
+            res.render("home", { userData: allData });
         }
     });
 });
 
-app.get("/adduser",function(req,res){
+app.get("/adduser", function (req, res) {
     res.render("newUser");
 })
 
-app.post("/adduser",function(req,res){
+app.post("/adduser", function (req, res) {
 
     // var newUser = {firstname:"drumil",lastname:"shah",age:"17",gender:"male",grade:"A",standard:"12"};
     // userdata.create(newUser,function(err,newuser){
-       Userdata.create(req.body.user,function(err,newuser){
-        if(err){
+    Userdata.create(req.body.user, function (err, newuser) {
+        if (err) {
             console.log(err);
-        }else{
+        } else {
             console.log("new user created");
             console.log(newuser);
             res.redirect("/");
@@ -39,22 +45,22 @@ app.post("/adduser",function(req,res){
     });
 });
 
-app.post("/search",function(req,res){
+app.post("/search", function (req, res) {
     var searchText = req.body.searchField;
     console.log(searchText);
     Userdata.find({
-        $or:[
-        {firstname:{$regex:searchText,$options:'i'}},{lastname:{$regex:searchText,$options:'i'}},{age:{$regex:searchText,$options:'i'}},{gender:{$regex:searchText,$options:'i'}},{grade:{$regex:searchText,$options:'i'}},{standard:{$regex:searchText,$options:'i'}}
+        $or: [
+            { firstname: { $regex: searchText, $options: 'i' } }, { lastname: { $regex: searchText, $options: 'i' } }, { age: { $regex: searchText, $options: 'i' } }, { gender: { $regex: searchText, $options: 'i' } }, { grade: { $regex: searchText, $options: 'i' } }, { standard: { $regex: searchText, $options: 'i' } }
         ]
-    },function(err,foundData){
-        if(err){
+    }, function (err, foundData) {
+        if (err) {
             console.log(err);
-        }else{
-        res.render("home",{userData:foundData});
+        } else {
+            res.render("home", { userData: foundData });
         }
     });
 });
 
-app.listen(3000,function(req,res){
+app.listen(process.env.port, function (req, res) {
     console.log('SERVER STARTED');
 });
